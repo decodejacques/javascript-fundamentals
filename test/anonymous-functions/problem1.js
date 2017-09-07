32320454374b84d70a42544ec1843a63233339ac
barbar <foo@foo.com>
2017-09-07 14:54:20 -0400

    Finished the anonymous function test

--- test/anonymous-functions/problem1.js
+++ test/anonymous-functions/problem1.js
@@ -2,11 +2,11 @@ var expect = require('chai').expect;
 
 
 
-var objects = require('../../src/__proto__/problem1.js');
+var t = require('../../src/anonymous-functions/problem1.js');
 
-describe('__proto__', function() {
-    it('The child should have the parent as its __proto__ ', function() {
-        expect(objects.child.__proto__).to.equal(objects.parent);
+describe('anonymous functions', function() {
+    it('', function() {
+        expect(t()).to.eql([7, 16]);
     });
 
 });
