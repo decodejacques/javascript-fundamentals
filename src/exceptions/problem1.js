a0b770964d13f02cae9133cd99cfb973b8f2bde2
Kamylle <kamylle.b.g@gmail.com>
2018-01-12 16:47:10 -0500

    Exceptions - Problem 1 - Done

--- src/exceptions/problem1.js
+++ src/exceptions/problem1.js
@@ -1,20 +1,36 @@
 function first(arr) {
-    // Throw an exception if the array has no elements
-    // Otherwise return the first element
+    if (arr.length == 0) {
+        throw new Error("No elements");
+    } else {
+        return arr[0];
+    }
+
 }
 
 function detective(i) {
     function suspect(i) {
         if(i * 7 % 3 == 0) throw new Error("Bad i!");
     }
-    // detective checks to see if the suspect throws an exception on input i.
-    // Returns "everything ok" if the suspect doesn't. 
-    // Returns "something fishy" if the suspect does.
+
+    try {
+        suspect(i) 
+        return "everything ok";
+    }
+    catch(e) {
+        return "something fishy";
+    }
 }
 
 function assignFlight(name) {
     var flightNumber = ((name.length * 7) % 20) + "0";
     var terrorSuspects = ["bob", "eric", "susie"];
+
+    for (var i = 0; i < terrorSuspects.length; i++) {
+        if (name == terrorSuspects[i]) {
+            throw new Error("Terrorist!");
+        }
+        return flightNumber;
+    }
     // if the name is a terror suspect, throw an exception
     // Otherwise, return the flight number
 }
