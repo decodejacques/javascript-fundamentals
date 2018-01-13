a36827c4a936f7af6f84b1ddf292854422d79ddc
jytsong <jyt.song@gmail.com>
2018-01-12 19:16:15 -0500

    jan12th

--- src/exceptions/problem1.js
+++ src/exceptions/problem1.js
@@ -1,22 +1,24 @@
 function first(arr) {
-    // Throw an exception if the array has no elements
-    // Otherwise return the first element
+    if (arr.length === 0) {throw "this array is empty";}
+    return arr[0];
 }
 
 function detective(i) {
     function suspect(i) {
         if(i * 7 % 3 == 0) throw new Error("Bad i!");
     }
-    // detective checks to see if the suspect throws an exception on input i.
-    // Returns "everything ok" if the suspect doesn't. 
-    // Returns "something fishy" if the suspect does.
+    try {suspect(i);}
+    catch(err) {
+        return('something fishy');
+    }
+    return 'everything ok';
 }
 
 function assignFlight(name) {
     var flightNumber = ((name.length * 7) % 20) + "0";
     var terrorSuspects = ["bob", "eric", "susie"];
-    // if the name is a terror suspect, throw an exception
-    // Otherwise, return the flight number
+    if(name == terrorSuspects[0] || name == terrorSuspects[1] || name == terrorSuspects[2]) {throw "suspect alert!";}
+    else return flightNumber;
 }
 
 module.exports = {first, detective, assignFlight}
