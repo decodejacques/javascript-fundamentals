3ef318f4962d2dbf13b62e41d5bd873381b84005
Kamylle <kamylle.b.g@gmail.com>
2018-01-15 18:06:58 -0500

    This Done

--- src/this/problem1.js
+++ src/this/problem1.js
@@ -1,5 +1,11 @@
 'use strict';
 function whatsMyAgeAgain() {
+    try {
+        return this.age;
+    }
+    catch(e) {
+        return 18;
+    }
     // returns this.age unless this is not defined. If this is not defined, return 18
 }
 
