6cf8873acdba5174a1b47373f4659b01b4a18d66
vbakarian <vako.bakarian@gmail.com>
2018-01-15 17:26:20 -0500

    Bind, anonymous functions and this problems

--- src/anonymous-functions/problem1.js
+++ src/anonymous-functions/problem1.js
@@ -1,12 +1,12 @@
 // fix all the errors
 function c(g, h) {
-    var x = g(6);
-    var y = h(8);
-    return [x, y];
+    var x = g(6); //sets g function with parameter 6 to the x variable
+    var y = h(8); // sets h function with parameter 8 to the y variable
+    return [x, y]; // returns an array with the variables x and y as parameters or return [g(6),h(8)]
 }
 
 function t() {
-    return c(function g(x) {return y + 1}, function h(y) {return x * 2});
+    return c(function g(x) {return x + 1}, function h(x) {return x * 2});
 }
 
 module.exports = t;
