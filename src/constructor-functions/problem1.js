26b3cdc8d549126eea71e27877837a72587c886b
Kamylle <kamylle.b.g@gmail.com>
2018-01-16 15:57:56 -0500

    Constructor Functions Done

--- src/constructor-functions/problem1.js
+++ src/constructor-functions/problem1.js
@@ -1,2 +1,4 @@
 // Add a function to all arrays called isNotEmpty
 // isNotEmpty returns true is the array is empty, false otherwise
+
+Array.prototype.isNotEmpty = function(){return this.length != 0};
