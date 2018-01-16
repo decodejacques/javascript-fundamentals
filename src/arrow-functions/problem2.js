ade4725168fc216b302b04cf2ca8a2dc421949e7
vbakarian <vako.bakarian@gmail.com>
2018-01-16 18:19:28 -0500

    Arrow functions, constructor functions, variable scoping

--- src/arrow-functions/problem2.js
+++ src/arrow-functions/problem2.js
@@ -1,10 +1,9 @@
 // Convert all the arrow functions to normal anonymous functions
 // There should be no arrows by the end
 
-var x = x => x + 1;
-var y = (x, y) => x + y;
-var z = x => {var y = (x * 7) % 2; return y * 2};
-
-module.exports = {x, y, z};
+var x = function (x) { return x + 1; };
+var y = function (x, y) { return x + y; };
+var z = function(x)  { var y = (x * 7) % 2; return y * 2 };
+module.exports = { x, y, z };
 
 
--- src/constructor-functions/problem1.js
+++ src/constructor-functions/problem1.js
@@ -1,2 +1,7 @@
 // Add a function to all arrays called isNotEmpty
 // isNotEmpty returns true is the array is empty, false otherwise
+var x = new Array();
+Array.prototype.isNotEmpty = function () { return this.length !== 0; }
+console.log(x.isNotEmpty())
+x.push(0);
+console.log(x.isNotEmpty())
