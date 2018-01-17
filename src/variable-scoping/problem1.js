9f9f7c032e0cee483f5bee5c60c8153ac10bd61e
Anouk <anoukmontreuil@gmail.com>
2018-01-16 19:49:19 -0500

    Completed JS files: 'Variable Scoping'

--- src/variable-scoping/problem1.js
+++ src/variable-scoping/problem1.js
@@ -3,10 +3,7 @@
 // The first time it is called it returns 1
 // Every call thereafter returns a number one greater than the last
 
-function f() {
-    var x = 0;  
-    x = x + 1;
-    return x;
-}
+let x = 0;
+function f() { x = x + 1; return x }
 
 module.exports = f;
