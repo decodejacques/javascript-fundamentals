bdf2f07e635dc6c5788cc7402997ca6f8916546e
Kamylle <kamylle.b.g@gmail.com>
2018-01-17 16:39:13 -0500

    Classes Done

--- src/classes/problem1.js
+++ src/classes/problem1.js
@@ -1,4 +1,10 @@
 class Dog {
+    constructor(age, name, breed) {
+        this.age = age;
+        this.name = name;
+        this.breed = breed;
+        this.bark = () => "woof";
+    }
     // Dog has a constructor with three arguments (in this order): age, name and breed
     // Dog has three attributes: age, name and breed
     // Dog has a method bark, which returns a string    
